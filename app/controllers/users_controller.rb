a7e3e660014c9d664fbd408a2745c2ae234ec8bc
Santiago Vargas <santdev404@gmail.com>
2021-04-15 09:11:54 -0500

    Edit user update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,8 @@
 class UsersController < ApplicationController
+
   def my_portafolio
+
+      @tracked_stocks = current_user.stocks
+
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   
+  resources :user_stocks, only: [:create, :destroy]
   devise_for :users
   root 'welcome#index'
   get 'my_portafolio', to: 'users#my_portafolio'
