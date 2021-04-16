7d1025969112947e03a4b9ff53faecaca0b311fb
Santiago Vargas <santdev404@gmail.com>
2021-04-16 08:59:58 -0500

    Today push

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,47 @@ class UsersController < ApplicationController
       @tracked_stocks = current_user.stocks
 
   end
+
+
+  def my_friends
+
+    @friendship = current_user.friends
+
+  end
+
+  def search
+
+      
+
+      if params[:friend].present?
+
+            @friends = User.search(params[:friend])
+
+            @friends = current_user.except_current_user(@friends)
+
+          if @friends
+
+              respond_to do |format|
+                  format.js { render partial: 'users/friend_result'}
+              end
+
+          else
+
+              respond_to do |format|
+                  flash.now[:alert] = "Couldn't find user"
+                  format.js { render partial: 'users/friend_result'}
+              end
+
+          end
+
+      else
+          respond_to do |format|
+              flash.now[:alert] = "Please enter a  friend name"
+              format.js { render partial: 'users/friend_result'}
+          end
+      end
+
+  end
+
+
 end
